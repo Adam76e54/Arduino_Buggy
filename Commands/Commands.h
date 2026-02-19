2635e2ba87b727723fb70452eef83126c5a94281
Adam <adam76gillesp.ie@gmail.com>
2026-02-19 15:48:41 +0000

    Changed codes

--- Commands/Commands.h
+++ Commands/Commands.h
@@ -5,27 +5,25 @@ namespace comm
   constexpr char DELIMITER = ':';//commands will look like "LEFT:SPEED:12.3"
   constexpr char END = '\n';
 
-  constexpr char BEAT = 'B';//GUI sends this every now and again to make sure we're connected and not stalling
+  constexpr const char* LEFT = "L";
+  constexpr const char* RIGHT = "R";
+  constexpr const char* SPEED = "S";
+  constexpr const char* IR = "I";
+  constexpr const char* ULTRA_SONIC = "O";
 
-  constexpr const char* LEFT = "LFT";
-  constexpr const char* RIGHT = "RGHT";
-  constexpr const char* SPEED = "SPD";
-  constexpr const char* IR = "IR";
-  constexpr const char* ULTRA_SONIC = "US";
-
-  constexpr const char* DIRECTION = "DRCT";
+  constexpr const char* DIRECTION = "Q";
   constexpr const char* W = "W";
   constexpr const char* D = "D";
   constexpr const char* A = "A";
   constexpr const char* S = "S";
-  constexpr const char* STOP = "STP";
-  constexpr const char* START = "SRT";
+  constexpr const char* STOP = "K";
+  constexpr const char* START = "B";
 
-  constexpr const char* MANUAL = "MNL";
-  constexpr const char* TRACK = "TRCK";
-  constexpr const char* FOLLOW = "FLLW";
+  constexpr const char* MANUAL = "M";
+  constexpr const char* TRACK = "T";
+  constexpr const char* FOLLOW = "F";
 
-  constexpr const char* EVENT = "EVNT";
+  constexpr const char* EVENT = "E";
 
   constexpr const char* REMOVED_MESSAGE = "Obstacle removed";
   constexpr const char* OBSTACLE_MESSAGE = "Stopped for obstacle";
